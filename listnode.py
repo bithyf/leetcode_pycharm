d166eccb4cac6bddacf45fb79c5597720c6a0b3c
huangyunfei <huang_yun_fei@126.com>
2020-03-03 20:54:01 +0800

    listnode.py

---
+++ listnode.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def listtolistnode(nums):
+    head = ListNode(nums[0])
+    p = head
+    for i in range(1, len(nums)):
+        node = ListNode(nums[i])
+        p.next = node
+        p = p.next
+
+    return head
+
+
+def printlistnode(head):
+    p = head
+    while p is not None:
+        print(p.val)
+        p = p.next
