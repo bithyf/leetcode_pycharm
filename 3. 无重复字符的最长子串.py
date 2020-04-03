fda3ca9a606dd4046a16431f402cbd7b0641f92b
huangyunfei <huang_yun_fei@126.com>
2020-04-03 11:31:51 +0800

    many

---
+++ 3. 无重复字符的最长子串.py
@@ -0,0 +1,21 @@
+def lengthOfLongestSubstring(s: str) -> int:
+    if len(s) < 2:
+        return s
+    maxlength = 0
+    left = right = 0
+    for n in s:
+        # 若n不在字符串里窗口大小加一
+        if n not in s[left:right]:
+            right += 1
+            if right - left > maxlength:
+                maxlength += 1
+        else:
+            left += s[left:right].index(n) + 1
+            right += 1
+
+    return maxlength
+
+
+S = "bbtablud"
+print(lengthOfLongestSubstring(S))
+
